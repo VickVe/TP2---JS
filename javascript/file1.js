40bd57a49a3ae6962c36cf5a850b4b63a0da7281
Victoria Verra <113646182+VickVe@users.noreply.github.com>
2022-11-03 01:31:29 -0300

    add js
    add js

---
+++ javascript/file1.js
@@ -0,0 +1,26 @@
+// Function Delete
+function fDelete(){
+    document.getElementById("myForm").reset();
+}
+
+// Function Calculate
+function fBuy(){
+    var entradas = document.getElementById("cantidad").value;
+    var total = entradas * 200;
+    var lista = document.getElementById("opciones");
+    var valorSeleccionado = lista.options[lista.selectedIndex].value;
+    var resultado = document.getElementById("total");
+    if (valorSeleccionado == "estudiante"){
+        total -= total * 0.80;
+        resultado.innerHTML = "Total a Pagar: $ " + total;
+    }else if (valorSeleccionado == "trainee"){
+        total -= total * 0.50;
+        resultado.innerHTML = "Total a Pagar: $ " + total;
+    }else if (valorSeleccionado == "junior"){
+        total -= total * 0.15;
+        resultado.innerHTML = "Total a Pagar: $ " + total;
+    }else if (valorSeleccionado == "otro"){
+        resultado.innerHTML = "Total a Pagar: $ " + total;
+    }
+}
+
